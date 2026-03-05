cb0612df924aa9b7c05dc3eae300beec88466000
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-03-05 11:49:46 +0700

    Add simple arithmetic Java project
    Add a new Eclipse Java project Bai3_Tinh_toan_don_gian with a basic Tinh_toan_don_gian.java demonstrating simple arithmetic (add, subtract, multiply, divide). Include IDE metadata (.classpath, .project, .settings) and a .gitignore to exclude /bin.

---
+++ HELLO_WORLD/Bai3_Tinh_toan_don_gian/src/Tinh_toan_don_gian.java
@@ -0,0 +1,17 @@
+
+public class Tinh_toan_don_gian {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+        int b = 5;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Cong: " + (a + b));
+        System.out.println("Tru: " + (a - b));
+        System.out.println("Nhan: " + (a * b));
+        System.out.println("Chia: " + (a / b));
+	}
+
+}
