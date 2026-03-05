e95c6e40bd6400573af2789dbce97c3ede2d27d8
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-03-05 08:30:00 +0700

    Add Hello_world Java app and Eclipse project
    Add a new Eclipse Java project 'Hello_world' including source (src/SayHelloApp.java) and compiled class (bin/SayHelloApp.class). SayHelloApp prints Vietnamese greetings and a simple arithmetic result. Project metadata and settings (.classpath, .project, .settings) are included and configured for Java SE 21.

---
+++ Hello_world/src/SayHelloApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Lệnh in ra màn hình
+		System.out.println("Xin chào thế giới Java");
+		System.out.println("Chào mừng đến với lập trình Java");
+		//In ra màn hình kết quả tính toán
+		System.out.println("5 + 3 = " + (5 + 3));
+	}
+
+}
