23a1fa59ecf6f63f978c0c6cbcf10bf0943ff4cd
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-03-05 10:52:26 +0700

    commit

---
+++ Bai1_In_thong_tin_ca_nhan/src/Thong_tin_ca_nhan.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Thong_tin_ca_nhan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Họ và tên SV: Dương Thành Tuấn Tú");
+		System.out.println("MSSV:66134852");
+		System.out.println("Ngày sinh: 10/07/2006");
+
+	}
+
+}
