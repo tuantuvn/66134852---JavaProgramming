a7f5da07cfb0d849e829a2c441f0c31099bbe8d2
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-03-05 11:42:58 +0700

    Add HELLO_WORLD project; update workspace metadata
    Add new HELLO_WORLD project 'Bai2_In_hinh_don_gian' with .project, .classpath, .settings, .gitignore (root and project), and src/In_hinh_don_gian.java (prints a simple ASCII triangle). Also update workspace/.metadata files (lock_info, log, version.ini, workbench.xmi, m2e log and resource safetable/tree entries) to reflect IDE state changes.

---
+++ HELLO_WORLD/Bai2_In_hinh_don_gian/src/In_hinh_don_gian.java
@@ -0,0 +1,13 @@
+
+public class In_hinh_don_gian {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("   *   ");
+	     System.out.println("  ***  ");
+	     System.out.println(" ***** ");
+	     System.out.println("*******");
+
+	}
+
+}
