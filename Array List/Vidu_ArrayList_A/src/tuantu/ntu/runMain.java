b8479395118beaa34b8eb1f739323fe06ac83eba
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-03-17 13:42:26 +0700

    Đang làm bài ví dụ câu a

---
+++ Array List/Vidu_ArrayList_A/src/tuantu/ntu/runMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package tuantu.ntu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class runMain {
+
+	public static void main(String[] args) {
+		//Khai báo một biến danh sách chứa các số nguyên
+		ArrayList<Integer> ds_so_Nguyen = new ArrayList<Integer>();
+		//a. Nhập 10 phần tử từ bàn phím
+		Scanner banphim = new Scanner(System.in);
+
+	}
+
+}
