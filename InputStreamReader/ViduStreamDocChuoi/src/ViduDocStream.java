c0bd42bca4b6a3645db8d21efe0ded301766bea3
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-04-13 21:00:14 +0700

    done

---
+++ InputStreamReader/ViduStreamDocChuoi/src/ViduDocStream.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class ViduDocStream {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fInput = new FileInputStream("noidung.txt");
+		
+		//Khai báo biến đối tượng InputStreamReader
+		InputStreamReader ipReader = new InputStreamReader(fInput);
+		//Khai báo biến đối tượng Buffee
+		BufferedReader buffReader = new BufferedReader(ipReader);
+		//In ra một câu hướng dẫn để User biết cần làm gì
+		//System.out.print("Nhập vào một chuỗi bất kì: ");
+		//Đọc đữ liệu từ file và cất biến vào chuỗi
+		String line1 = buffReader.readLine();
+		System.out.print(line1);
+		
+
+	}
+
+}
