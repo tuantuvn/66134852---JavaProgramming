f5b27321d0e459517018f9db3fe2437185cb8df2
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-04-22 15:44:59 +0700

    Update Quan_Ly_Sinh_Vien.java

--- InputStreamReader/SinnVien/src/thigk2/Duong_Thanh_Tuan_Tu/Quan_Ly_Sinh_Vien.java
+++ InputStreamReader/SinnVien/src/thigk2/Duong_Thanh_Tuan_Tu/Quan_Ly_Sinh_Vien.java
@@ -1,5 +1,14 @@
 package thigk2.Duong_Thanh_Tuan_Tu;
 
-public class Quan_Ly_Sinh_Vien {
+//Tạo lớp "Quản lý Sinh Viên" kế thừa từ lớp "SinhVien"
+public class Quan_Ly_Sinh_Vien extends SinhVien {
+
+	public Quan_Ly_Sinh_Vien(int sTT, String ho_va_ten, int namSinh, String gioiTinh) {
+		super(sTT, ho_va_ten, namSinh, gioiTinh);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	
 
 }
