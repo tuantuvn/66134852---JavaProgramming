42c9ddb5d827afedec1de79a01a275d48cb2c235
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-04-23 08:02:54 +0700

    LẦN THỨ 4

--- Thi GK 2/Cau2/src/thigk2/DuongThanhTuanTu/Cau2a.java
+++ Thi GK 2/Cau2/src/thigk2/DuongThanhTuanTu/Cau2a.java
@@ -1,5 +1,49 @@
 package thigk2.DuongThanhTuanTu;
-
+//Câu 2a
 public class Cau2a {
-
+	protected int MSSV;
+	protected String HoVaten;
+	protected String GioiTinh;
+	protected int NamSinh;
+	//Xây dựng hàm getter và setter
+	public int getMSSV() {
+		return MSSV;
+	}
+	public void setMSSV(int mSSV) {
+		MSSV = mSSV;
+	}
+	public String getHoVaten() {
+		return HoVaten;
+	}
+	public void setHoVaten(String hoVaten) {
+		HoVaten = hoVaten;
+	}
+	public String getGioiTinh() {
+		return GioiTinh;
+	}
+	public void setGioiTinh(String gioiTinh) {
+		GioiTinh = gioiTinh;
+	}
+	public int getNamSinh() {
+		return NamSinh;
+	}
+	public void setNamSinh(int namSinh) {
+		NamSinh = namSinh;
+	}
+	//Xây dựng hàm Constructor
+	public Cau2a(int mSSV, String hoVaten, String gioiTinh, int namSinh) {
+		super();
+		MSSV = mSSV;
+		HoVaten = hoVaten;
+		GioiTinh = gioiTinh;
+		NamSinh = namSinh;
+	}
+	//Xây dựng hàm toString
+	@Override
+	public String toString() {
+		return "Cau2a [MSSV=" + MSSV + ", HoVaten=" + HoVaten + ", GioiTinh=" + GioiTinh + ", NamSinh=" + NamSinh + "]";
+	}
+	
+	
+	
 }
---
+++ Thi GK 2/Cau2/src/thigk2/DuongThanhTuanTu/Cau2b.java
@@ -0,0 +1,10 @@
+package thigk2.DuongThanhTuanTu;
+
+public class Cau2b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
