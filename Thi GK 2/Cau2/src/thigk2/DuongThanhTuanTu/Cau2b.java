ab1affb4ed6de4a2e7c4f174205771faf0ff5560
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-04-23 08:10:29 +0700

    LẦN THỨ 5

--- Thi GK 2/Cau2/src/thigk2/DuongThanhTuanTu/Cau2b.java
+++ Thi GK 2/Cau2/src/thigk2/DuongThanhTuanTu/Cau2b.java
@@ -1,10 +1,26 @@
 package thigk2.DuongThanhTuanTu;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cau2b {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Tạo ra danh sách 3 sinh viên
+		ArrayList <Cau2a> dssv = new ArrayList<>();
+		Cau2a sv1 = new Cau2a(6612, "Trần Đức Chiến", "Nam", 2007);
+		Cau2a sv2 = new Cau2a(6623, "Nguyễn Văn Nam", "Nam", 2002);
+		Cau2a sv3 = new Cau2a(6612, "Trần Đức Chiến", "Nam", 2003);
+		
+		//In ra danh sách sinh viên mới vừa thêm
+		for (Cau2a sv:dssv) {
+			System.out.println(sv.toString());
+		}
+		//Thêm mới một sinh viên từ bàn phím
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập thêm thông tin sinh viên mới");
+		System.out.print("Nhập mã số sinh viên: ");
+		int mssv = sc.nextInt();
+		System.out.print();
 	}
 
 }
