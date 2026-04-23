849aa6d1f8b94a95801170438380c0158539012e
Duong Thanh Tuan Tu <duongthanhtuantu1007@gmail.com>
2026-04-23 07:41:32 +0700

    LẦN THỨ 1

---
+++ Thi GK 2/Cau1/src/thigk2/DuongThanhTuanTu/Cau1.java
@@ -0,0 +1,18 @@
+package thigk2.DuongThanhTuanTu;
+import java.util.Scanner;
+public class Cau1 {
+
+	public static void main(String[] args) {
+		//Khai báo lớp Scanner, viết thống báo và khai báo biến
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập thông tin chiều cao (m): ");
+		double chieucao = sc.nextDouble();
+		System.out.print("Nhập thông tin cân nặng (kg): ");
+		double cannang = sc.nextDouble();
+		double BMI = (cannang)/(chieucao*chieucao);
+		System.out.println("Chỉ số BMI của bạn là: "+BMI);
+		
+
+	}
+
+}
